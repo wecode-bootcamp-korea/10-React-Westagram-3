4a72a5db3a89da1df5de35be0723e355ded7b91e
limo-sum <i01029407043@gmail.com>
2020-07-09 21:43:22 +0900

    Refactor: 로그인 페이지 기능 구현

--- src/Pages/JIYUNLEE/Login/Login.js
+++ src/Pages/JIYUNLEE/Login/Login.js
@@ -3,28 +3,70 @@ import { withRouter } from "react-router-dom";
 import "./Login.scss";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+  handleEmailChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+  handleEnterLogin = (e) => {
+    if (this.canBeSubmitted() && e.keyCode === 13) {
+      this.goToMain();
+    }
+  };
+  canBeSubmitted() {
+    const { email, password } = this.state;
+    return email.includes("@") && password.length > 5;
+  }
   goToMain() {
+    {
+      console.log(this.state.email);
+    }
+    {
+      console.log(this.state.password);
+    }
     this.props.history.push("/jiyunMain");
   }
 
   render() {
+    const isEnabled = this.canBeSubmitted();
     return (
       <div class="Login_JY">
         <div className="box">
           <div className="box1">
-            <img
-              className="logo"
-              src="/images/JIYUNLEE/logo_text.png"
-              width="175px"
-            />
+            <img className="logo" src="/images/JIYUNLEE/logo_text.png" />
             <form className="inputLogin">
               <input
                 type="text"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
+                onKeyUp={this.handleEnterLogin}
                 autocomplete="off"
               />
-              <input type="password" id="inputpw" placeholder="비밀번호" />
-              <button onClick={this.goToMain.bind(this)} type="button">
+              <input
+                type="password"
+                id="inputpw"
+                placeholder="비밀번호"
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
+                onKeyUp={this.handleEnterLogin}
+              />
+              <button
+                disabled={!isEnabled}
+                onClick={this.goToMain.bind(this)}
+                style={{
+                  backgroundColor: isEnabled ? "skyblue" : "lightblue",
+                }}
+                type="button"
+              >
                 로그인
               </button>
             </form>
