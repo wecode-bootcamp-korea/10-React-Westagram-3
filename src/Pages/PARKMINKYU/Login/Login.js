4c5a88695c957de3b94afb58a3da8af2d0a5e896
7p3m1k <kor0885@naver.com>
2020-07-13 11:37:50 +0900

    Add: 로그인 기능 구현 추가 및 히스토리 삭제

--- src/Pages/PARKMINKYU/Login/Login.js
+++ src/Pages/PARKMINKYU/Login/Login.js
@@ -2,6 +2,32 @@ import React from "react";
 import "./Login.scss";
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  getColsoleValue = () => {
+    console.log(
+      "아이디 :",
+      this.state.email,
+      "패스워드 :",
+      this.state.password
+    );
+    this.props.history.push("/parkminkyuMain");
+  };
+
   render() {
     return (
       <div className="Login_MK">
@@ -12,9 +38,28 @@ class Login extends React.Component {
           className="loginId"
           type="text"
           placeholder="전화번호,사용자 이름 또는 이메일"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
         />
-        <input className="loginPw" type="password" placeholder="비밀번호" />
-        <button className="loginbutton">로그인</button>
+        <input
+          className="loginPw"
+          type="password"
+          placeholder="비밀번호"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <button
+          className="loginbutton"
+          onClick={this.getColsoleValue}
+          style={{
+            backgroundColor:
+              this.state.email.includes("@") && this.state.password.length > 5
+                ? "rgba(0, 149, 246, 1)"
+                : "",
+          }}
+        >
+          로그인
+        </button>
         <p className="lossPw">비밀번호를 잊으셨나요?</p>
       </div>
     );
